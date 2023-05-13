d142b4deaaa9cf574d07697e6a3ff77bc4e391f8
XX-V-MCM.LXX.Vl <2442919@protonmail.com>
2023-05-13 19:45:11 +0200

    Add files via upload

---
+++ HtmlTags.js
@@ -0,0 +1,41 @@
+function area(obj, text) {
+    obj.innerText = text;
+}
+function base(obj, text) {
+    obj.innerText = text;
+}
+function basefont(obj, text) {
+    obj.innerText = text;
+}
+function br(obj, text) {
+    obj.innerText = text;
+}
+function col(obj, text) {
+    obj.innerText = text;
+}
+function frame(obj, text) {
+    obj.innerText = text;
+}
+function hr(obj, text) {
+    obj.innerText = text;
+}
+function img(obj, text) {
+    obj.innerText = text;
+}
+function input(obj, text) {
+    obj.innerText = text;
+}
+function link(obj, text) {
+    obj.innerText = text;
+}
+function meta(obj, text) {
+    obj.innerText = text;
+}
+function param(obj, text) {
+    obj.innerText = text;
+}
+function p(obj, text) {
+    obj.innerText = text;
+}
+
+
